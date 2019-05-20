1239f9c5e7247d30bb69276370571752438ce9f1
ejay010 <ejay010@gmail.com>
2019-05-20 07:11:34 -0400

    asdfasd

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,68 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+<b-container>
+  <b-row>
+    <b-col>
+      <b-card>
+        <b-form>
+          <b-form-group label="Estimated Cost of your Dream Home">
+            <money v-model="cost" v-bind="money" class="form-control"></money>
+          </b-form-group>
+
+          <b-form-group label="Interest Rate %">
+            <b-form-input type="number" required placeholder="3.99" v-model="rate"></b-form-input>
+          </b-form-group>
+
+          <b-form-group label="Course of Mortgage (years)">
+            <b-form-input type="number" disabled placeholder="30" v-model="time"></b-form-input>
+          </b-form-group>
+        </b-form>
+      </b-card>
+    </b-col>
+    <b-col>
+      <b-card>
+        <h3>Down Payment @ 15%: ${{downpayment.toLocaleString("latn", {
+            style: 'currency',
+            currency: 'BSD'
+          })}}</h3>
+        <h3>Monthly Payment: ${{MonthlyPayment.toLocaleString("latn", {
+            style: 'currency',
+            currency: 'BSD'
+          })}} </h3>
+      </b-card>
+    </b-col>
+  </b-row>
+</b-container>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
+  data() {
+    return {
+      cost: 250000,
+      rate: 3.99,
+      time: 30,
+      money: {
+        decimal: '.',
+        thousands: ',',
+        prefix: 'B$ ',
+        suffix: '',
+        precision: 2,
+        masked: false /* doesn't work with directive */
+      }
+    }
+  },
+  computed: {
+    downpayment() {
+      return (this.cost * 0.15)
+    },
+    MonthlyPayment() {
+      return (((this.rate / 100) / 12) * this.cost)
+    }
   }
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+import BoostrapVue from 'bootstrap-vue'
+Vue.use(BoostrapVue)
+
+import money from 'v-money'
+Vue.use(money, {precision: 4})
+
 new Vue({
   router,
   store,
